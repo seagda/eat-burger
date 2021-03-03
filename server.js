92f2591602f816d7782ae81f8f7efdd5c83ecada
Shea Mullaney <seagda@gmail.com>
2021-03-03 09:21:18 -0800

    setting up server and package files

---
+++ server.js
@@ -0,0 +1,30 @@
+// setting up required db and server packages
+var express = require("express");
+var db = require("./models");
+
+var app = express();
+
+app.use(express.static("public"));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controller");
+
+app.use(routes);
+
+// get the app listening for requests
+var PORT = process.env.PORT || 3000;
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App now listening on port:", PORT);
+  });
+});
